35adb86d2f8fe31b77996a4ffb57a8808e559f12
taward31 <taward31@gmail.com>
2022-09-19 11:47:03 +0000

    create randome generators

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// Wait for the Page to load prior to enableing buttons/functions 
+
+document.addEventListener("DOMContentLoaded",function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click",function() {
+            if (this.getAttribute("data-type")=== "submit") {
+                alert("you click submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert (`You clicked ${gameType}`);
+            }
+        })
+    }
+            
+    })
+
+
 
 function runGame() {
 
